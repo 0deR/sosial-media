9269e5d279c4798a2991051a3a09ee14a2182226
sofhila wahiddin <crawsculeswahiddin@gmail.com>
2021-07-26 11:48:10 +0700

    change h1

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-8">
-                        <h1>Selamat datang di Pesbuk</h1>
+                        <h1>Selamat datang di facebook</h1>
                         <p>Mari Bergabung bersama jutaan orang lainnya...</p>
                     </div>
                     <div class="col-md-4">
